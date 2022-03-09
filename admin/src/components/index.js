aa247efda797d483a56833d064d6c066211e196a
Matt Milburn <me@mattmilburn.com>
2022-03-09 15:38:55 -0600

    Copy paste Strapis InputUID to use alongside PermalinkUID

--- admin/src/components/index.js
+++ admin/src/components/index.js
@@ -1,2 +1,4 @@
+export { default as Field } from './Field';
 export { default as Initializer } from './Initializer';
 export { default as InputUID } from './InputUID';
+export { default as PermalinkUID } from './PermalinkUID';
