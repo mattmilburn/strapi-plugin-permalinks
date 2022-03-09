b8375ac53ee10e0fd3d2f00f6896b398f0306be8
Matt Milburn <me@mattmilburn.com>
2022-03-09 17:51:03 -0600

    Add validation for permalinkRelation prop

--- admin/src/components/Field/index.js
+++ admin/src/components/Field/index.js
@@ -4,16 +4,22 @@ import PropTypes from 'prop-types';
 import { InputUID, PermalinkUID } from '../';
 
 const Field = props => {
-  if ( props.attribute?.permalinkMode === true ) {
-    return <PermalinkUID { ...props } />;
+  const target = props.attribute?.permalinkRelation;
+  const attr = target && props.attribute[ target ];
+  const attrIsValid = attr && attr.type === 'relation';
+
+  // If permalink settings are not enabled, fallback to Strapi's core InputUID.
+  if ( ! attrIsValid ) {
+    return <InputUID { ...props } />;
   }
 
-  return <InputUID { ...props } />;
+  // Maybe use custom UID field to work with permalinks.
+  return <PermalinkUID { ...props } />;
 };
 
 Field.propTypes = {
   attribute: PropTypes.shape( {
-    permalinkMode: PropTypes.bool,
+    permalinkRelation: PropTypes.bool,
   } ).isRequired,
 };
 
