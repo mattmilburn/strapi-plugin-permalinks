970bf51eb449d9291714482cc55d7f18a7d1951f
Matt Milburn <me@mattmilburn.com>
2022-03-16 18:17:17 -0500

    Add schema validation for plugin options

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,14 +1,41 @@
 'use strict';
 
-module.exports = ( { strapi } ) => {
-  // TBD
+const { ValidationError } = require('@strapi/utils').errors;
+
+const { getService } = require( './utils' );
+
+module.exports = async ( { strapi } ) => {
+  const { contentTypes } = await getService( 'permalinks' ).getConfig();
+  const models = contentTypes.map( type => type.uid );
+
+  contentTypes.forEach( type => {
+    const model = strapi.db.metadata.get( type.uid );
+
+    if ( ! model ) {
+      throw new ValidationError( `The content type ${type.uid} does not exist.` );
+    }
+
+    const { attributes } = model;
+
+    if ( ! attributes[ type.targetField ] ) {
+      throw new ValidationError( `The target field ${type.targetField} is not defined for ${type.uid}.` );
+    }
+
+    if ( ! attributes[ type.targetRelation ] ) {
+      throw new ValidationError( `The target relation ${type.targetRelation} is not defined for ${type.uid}.` );
+    }
+  } );
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  // Lifecycle hook to sync descendents after a parent relation changes.
   const afterUpdate = event => {
-    console.log( event );
-    // @TODO - Find other entities that use this one as their parent and update their permalinks.
+    console.log( 'AFTER UPDATE', event );
   };
 
   // Subscribe to lifecycle hook.
   strapi.db.lifecycles.subscribe( {
+    models,
     afterUpdate,
   } );
 };
