287d2e3594075c0d313d9f3cf33a4069143682ad
Matt Milburn <me@mattmilburn.com>
2022-03-16 13:20:27 -0500

    Add styles for column text

--- admin/src/constants.js
+++ admin/src/constants.js
@@ -1,5 +1,4 @@
 import { pluginId } from './utils/plugin-id';
 
 export const PATH_DELIMITER = '~';
-export const PATH_REPLACE_REGEX = new RegExp( /~/, 'g' );
 export const RESOLVE_CONFIG = `${pluginId}/resolve-config`;
