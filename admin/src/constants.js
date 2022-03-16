4aa1cfeec9394c7a4178e957203ceae19998ed27
Matt Milburn <me@mattmilburn.com>
2022-03-16 13:02:28 -0500

    Add custom hook to update permalink columns in list view

--- admin/src/constants.js
+++ admin/src/constants.js
@@ -1,4 +1,5 @@
 import { pluginId } from './utils/plugin-id';
 
 export const PATH_DELIMITER = '~';
+export const PATH_REPLACE_REGEX = new RegExp( /~/, 'g' );
 export const RESOLVE_CONFIG = `${pluginId}/resolve-config`;
