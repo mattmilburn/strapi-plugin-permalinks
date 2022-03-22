64962fe2167208f6ad29e6bb4961892681c19f70
Matt Milburn <me@mattmilburn.com>
2022-03-22 17:49:33 -0500

    Rename syncDescendants to syncChildren

--- server/bootstrap/lifecycle-before-update.js
+++ server/bootstrap/lifecycle-before-update.js
@@ -25,7 +25,7 @@ module.exports = async ( { strapi } ) => {
       return;
     }
 
-    await pluginService.syncDescendants(
+    await pluginService.syncChildren(
       model.uid,
       where.id,
       nextValue,
