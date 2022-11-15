72e0f6186f1a9e2171cd8809811ea94e3b741646
Matt Milburn <me@mattmilburn.com>
2022-11-15 15:42:14 -0600

    Create getRelationValue util

--- admin/src/utils/index.js
+++ admin/src/utils/index.js
@@ -2,6 +2,7 @@ export { default as axiosInstance } from './axios-instance';
 export { default as getPermalink } from './get-permalink';
 export { default as getPermalinkAncestors } from './get-permalink-ancestors';
 export { default as getPermalinkSlug } from './get-permalink-slug';
+export { default as getRelationValue } from './get-relation-value';
 export { default as getTrad } from './get-trad';
 export { default as pluginId } from './plugin-id';
 export { default as pluginName } from './plugin-name';
