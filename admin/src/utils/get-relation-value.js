5d94419e33a9b0f6a4e76eeb4aac355e4525134f
Matt Milburn <me@mattmilburn.com>
2022-11-21 13:46:10 -0600

    Fix getRelationValue handling of array type

--- admin/src/utils/get-relation-value.js
+++ admin/src/utils/get-relation-value.js
@@ -1,15 +1,11 @@
 const getRelationValue = ( data, key ) => {
   const value = data[ key ];
 
-  if ( ! value || ! value.length ) {
-    return null;
-  }
-
   if ( Array.isArray( value ) ) {
     return value[ 0 ];
   }
 
-  return value;
+  return value ?? null;
 };
 
 export default getRelationValue;
