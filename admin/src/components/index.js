54b935afc576695320b92c0f737252e3e5196215
Matt Milburn <me@mattmilburn.com>
2022-12-27 14:56:41 -0600

    Move InputUID to coreComponents directory

--- admin/src/components/index.js
+++ admin/src/components/index.js
@@ -1,5 +1,4 @@
 export { default as Field } from './Field';
 export { default as Initializer } from './Initializer';
-export { default as InputUID } from './InputUID';
 export { default as ListViewTableCell } from './ListViewTableCell';
 export { default as PermalinkUID } from './PermalinkUID';
---
+++ admin/src/coreComponents/index.js
@@ -0,0 +1,2 @@
+// The following components were cloned from Strapi's core files with minor mods.
+export { default as InputUID } from './InputUID';
