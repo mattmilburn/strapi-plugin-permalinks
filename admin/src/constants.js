35f49ec141061fe636a4f5f8964a740812a0bb82
Matt Milburn <me@mattmilburn.com>
2023-01-02 15:50:49 -0600

    Re-organize some vars

--- admin/src/constants.js
+++ admin/src/constants.js
@@ -1,5 +1,8 @@
 import pluginId from './utils/plugin-id';
 
+export const ACTION_RESOLVE_CONFIG = `${pluginId}/resolve-config`;
+
 export const PATH_SEPARATOR = '~';
 export const SLASH_SEPARATOR = '/';
-export const RESOLVE_CONFIG = `${pluginId}/resolve-config`;
+
+export const UID_REGEX = /^[A-Za-z0-9-_.~]*$/;
