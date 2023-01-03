c059ee7b1312ce8d9105e9f922bc171178d46f57
Matt Milburn <me@mattmilburn.com>
2023-01-03 11:44:48 -0600

    Refactor schema validation for new config format

--- server/bootstrap/schema-validation.js
+++ server/bootstrap/schema-validation.js
@@ -5,23 +5,49 @@ const { ValidationError } = require( '@strapi/utils' ).errors;
 const { getService } = require( '../utils' );
 
 module.exports = async ( { strapi } ) => {
-  const { contentTypes } = await getService( 'config' ).get();
+  const { contentTypes2 } = await getService( 'config' ).get();
+  const uids = contentTypes2.flat();
 
-  contentTypes.forEach( type => {
-    const model = strapi.db.metadata.get( type.uid );
+  uids.forEach( uid => {
+    const model = strapi.db.metadata.get( uid );
 
     if ( ! model ) {
-      throw new ValidationError( `The content type ${type.uid} does not exist.` );
+      throw new ValidationError( `The content type ${uid} does not exist.` );
     }
 
     const { attributes } = model;
 
-    if ( ! attributes[ type.targetField ] ) {
-      throw new ValidationError( `The target field ${type.targetField} is not defined for ${type.uid}.` );
+    // Ensure that exactly one permalink attribute is defined for this model.
+    const permalinkAttrs = Object.entries( attributes ).filter( ( [ _, attr ] ) => {
+      return attr.customField === 'plugin::permalinks.permalink';
+    } );
+
+    if ( permalinkAttrs.length !== 1 ) {
+      throw new ValidationError( `Must have exactly one permalink attribute defined in ${uid}.` );
     }
 
-    if ( ! attributes[ type.targetRelation ] ) {
-      throw new ValidationError( `The target relation ${type.targetRelation} is not defined for ${type.uid}.` );
+    // Ensure that permalink attributes have required props defined.
+    const [ permalinkName, permalinkAttr ] = permalinkAttrs[ 0 ];
+
+    if ( ! permalinkAttr.targetField ) {
+      throw new ValidationError( `Missing ${permalinkName}.targetField in ${uid}.` );
+    }
+
+    if ( ! permalinkAttr.targetRelation ) {
+      throw new ValidationError( `Missing ${permalinkName}.targetRelation in ${uid}.` );
+    }
+
+    // Ensure that permalink attributes target an actual relation field type and the UID exists.
+    const targetRelationAttr = attributes[ permalinkAttr.targetRelation ];
+
+    if ( targetRelationAttr.type !== 'relation' ) {
+      throw new ValidationError( `Must use a valid relation type for ${permalinkName}.targetRelation in ${uid}.` );
+    }
+
+    const targetRelationModel = strapi.db.metadata.get( targetRelationAttr.target );
+
+    if ( ! targetRelationModel ) {
+      throw new ValidationError( `The content type ${targetRelationAttr.target} does not exist.` );
     }
   } );
 };
