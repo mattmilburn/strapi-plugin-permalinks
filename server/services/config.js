bd14e0fc6926fd05f2ac29269aa88fb7e23b86a9
Matt Milburn <me@mattmilburn.com>
2023-01-06 14:44:56 -0600

    Refactor how config provides layout data for each configured UID permalink field

--- server/services/config.js
+++ server/services/config.js
@@ -1,20 +1,36 @@
 'use strict';
 
-const config = require( '../config' );
+const get = require( 'lodash/get' );
+
+const { default: defaultConfig } = require( '../config' );
 const { getService, pluginId } = require( '../utils' );
 
 module.exports = ( { strapi } ) => ( {
   async get() {
+    const config = await strapi.config.get( `plugin.${pluginId}`, defaultConfig );
+
+    return config;
+  },
+
+  async layouts() {
     const pluginService = getService( 'permalinks' );
-    const { contentTypes } = await strapi.config.get( `plugin.${pluginId}`, config.default );
+    const config = await strapi.config.get( `plugin.${pluginId}`, defaultConfig );
+    const { contentTypes } = config;
 
-    // Add `layouts` data to config based on `contentTypes`.
+    // Add `layouts` data to config based on content types with a permalink field configured.
     const layouts = contentTypes.flat().reduce( ( acc, uid ) => {
-      const [ name, attr ] = pluginService.getPermalinkAttr( uid );
+      const model = pluginService.getModel( uid );
 
-      if ( ! name ) {
+      const permalinkAttr = Object.entries( model.attributes ).find( ( [ _name, attr ] ) => {
+        return attr.customField === 'plugin::permalinks.permalink';
+      } );
+
+      if ( ! permalinkAttr ) {
         return acc;
       }
+
+      const [ name, attr ] = permalinkAttr;
+      const relationUID = get( model, [ 'attributes', attr.targetRelation, 'target' ] );
 
       return {
         ...acc,
@@ -22,13 +38,11 @@ module.exports = ( { strapi } ) => ( {
           name,
           targetField: attr.targetField,
           targetRelation: attr.targetRelation,
+          targetRelationUID: relationUID,
         },
       };
     }, {} );
 
-    return {
-      contentTypes,
-      layouts,
-    };
+    return layouts;
   },
 } );
