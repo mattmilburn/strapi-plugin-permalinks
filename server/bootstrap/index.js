b97fe30497298e7eb778e44495d6edb729a93e0b
Matt Milburn <me@mattmilburn.com>
2023-01-09 20:50:34 -0600

    Move lifecycles to their own folder

--- server/bootstrap/index.js
+++ server/bootstrap/index.js
@@ -1,11 +1,15 @@
 'use strict';
 
+const beforeUpdateLifecycle = require( '../lifecycles/before-update' );
 const transformApiResponseMiddleware = require( '../middlewares/transform-api-response' );
-const lifecycleBeforeUpdate = require( './lifecycle-before-update' );
 const schemaValidation = require( './schema-validation' );
 
 module.exports = async params => {
-  await transformApiResponseMiddleware( params );
   await schemaValidation( params );
-  await lifecycleBeforeUpdate( params );
+
+  // Middlewares.
+  await transformApiResponseMiddleware( params );
+
+  // Lifecycles.
+  await beforeUpdateLifecycle( params );
 };
