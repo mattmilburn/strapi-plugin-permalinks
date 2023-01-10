42efb386b9fe3a7ee3d994b374368fb433fd2a4f
Matt Milburn <me@mattmilburn.com>
2023-01-09 21:12:58 -0600

    Add copy link button to edit view sidebar

---
+++ admin/src/components/EditViewRightLinks/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useCMEditViewDataManager } from '@strapi/helper-plugin';
+
+import { pluginId } from '../../utils';
+import { CopyLinkButton } from '../';
+
+const EditViewRightLinks = () => {
+  const {
+    allLayoutData,
+    isCreatingEntry,
+    modifiedData,
+  } = useCMEditViewDataManager();
+  const { config, isLoading } = useSelector( state => state[ `${pluginId}_config` ] );
+  const { uid } = allLayoutData.contentType;
+
+  if ( isLoading || isCreatingEntry ) {
+    return null;
+  }
+
+  const attr = config.layouts[ uid ];
+  const url = modifiedData[ attr.name ];
+
+  return <CopyLinkButton url={ url } />;
+};
+
+export default EditViewRightLinks;
