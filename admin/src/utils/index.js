d81fe7a4b6f58b1348f2ece7c311ccca3a5d1e6d
Matt Milburn <me@mattmilburn.com>
2023-01-16 15:07:39 -0600

    Create parseUrl util

--- admin/src/utils/index.js
+++ admin/src/utils/index.js
@@ -5,6 +5,7 @@ export { default as getPermalinkSlug } from './get-permalink-slug';
 export { default as getRelationValue } from './get-relation-value';
 export { default as getTrad } from './get-trad';
 export { default as interpolate } from './interpolate';
+export { default as parseUrl } from './parse-url';
 export { default as pluginId } from './plugin-id';
 export { default as pluginName } from './plugin-name';
 export { default as trimSlashes } from './trim-slashes';
