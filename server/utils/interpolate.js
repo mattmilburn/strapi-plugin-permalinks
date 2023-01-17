851534ad0cea70fc13b81aaf324b9f13a443de37
Matt Milburn <me@mattmilburn.com>
2023-01-17 14:58:45 -0600

    Create server utils for interpolate, trimSlashes, and parseUrl

---
+++ server/utils/interpolate.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const interpolate = ( str, data = {} ) => {
+  Object.entries( data ).forEach( ( [ key, value ] ) => {
+    str = str.replace( new RegExp( `{${key}}`, 'g' ), value );
+  } );
+
+  // Replace any remaining values with an empty string.
+  str = str.replace( new RegExp( `{(.*)}`, 'g' ), '' );
+
+  return str;
+};
+
+module.exports = interpolate;
