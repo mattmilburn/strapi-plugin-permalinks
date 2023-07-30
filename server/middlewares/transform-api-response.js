0f17715f5126a538cb0fe2d4923f8cfe37a1e7ac
Matt Milburn <me@mattmilburn.com>
2023-07-30 15:11:13 -0500

    Refactor recursive logic with transform middleware

--- server/middlewares/transform-api-response.js
+++ server/middlewares/transform-api-response.js
@@ -3,7 +3,7 @@
 const get = require( 'lodash/get' );
 const has = require( 'lodash/has' );
 const head = require( 'lodash/head' );
-const isArray = require( 'lodash/isArray' );
+const omit = require('lodash/omit');
 const set = require( 'lodash/set' );
 
 const { PATH_SEPARATOR, SLASH_SEPARATOR } = require( '../constants' );
@@ -24,28 +24,49 @@ module.exports = ( { strapi } ) => {
     const { url } = config.contentTypes.find( item => item.uids.includes( uid ) );
     const { name, targetRelation, targetRelationUID } = layout;
 
-    if ( ! uid ) {
+    if ( ! data || ! uid ) {
       return data;
     }
 
-    // Single entry.
-    if ( has( data, 'attributes' ) ) {
-      return transform( data.attributes, uid, config );
+    // Entity or relation wrapper.
+    if ( has( data, 'data' ) ) {
+      return {
+        ...data,
+        data: transform( data.data, uid, config ),
+      };
     }
 
-    // Collection of entries.
-    if ( isArray( data ) && data.length ) {
-      const firstItem = head( data );
+    // Entity or relation data.
+    if ( has( data, 'attributes' ) ) {
+      return {
+        ...data,
+        attributes: transform( data.attributes, uid, config ),
+      };
+    }
 
-      if ( has( firstItem, 'attributes' ) || has( firstItem, name ) ) {
-        return data.map( item => transform( item, uid, config ) );
-      }
+    // Support strapi-plugin-transformer features which might remove `attributes`
+    // and `data` wrappers.
+    if ( has( data, 'id' ) ) {
+      return {
+        ...data,
+        // Must omit the id otherwise it creates an infinite loop.
+        ...transform( omit( data, 'id' ), uid, config ),
+      };
+    }
+
+    // Collection of entities, relations, or components.
+    if ( Array.isArray( data ) && (
+      has( head( data ), 'attributes' ) ||
+      has( head( data ), 'id' ) ||
+      has( head( data ), '__component' )
+    ) ) {
+      return data.map( item => transform( item, uid, config ) );
     }
 
     // Maybe transform entries from the `localizations` array, which may only
     // apply if the API request populated the localized entries.
     if ( has( data, 'localizations' ) ) {
-      data.localizations = data.localizations.map( entry => transform( entry, uid, config ) );
+      data.localizations = transform( data.localizations, uid, config );
     }
 
     // Maybe sanitize the `targetRelation` permalink field, which may only apply
