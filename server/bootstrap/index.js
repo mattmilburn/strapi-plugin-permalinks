e8c333e3a3d6376184735da6139ad170925ea67d
Matt Milburn <me@mattmilburn.com>
2023-07-31 10:44:01 -0500

    Remove middleware that replaces ~ with /

--- server/bootstrap/index.js
+++ server/bootstrap/index.js
@@ -1,15 +1,9 @@
 'use strict';
 
 const afterUpdateLifecycle = require( '../lifecycles/after-update' );
-const transformApiResponseMiddleware = require( '../middlewares/transform-api-response' );
 const schemaValidation = require( './schema-validation' );
 
 module.exports = async params => {
   await schemaValidation( params );
-
-  // Middlewares.
-  await transformApiResponseMiddleware( params );
-
-  // Lifecycles.
   await afterUpdateLifecycle( params );
 };
