50a65637f3063e2519d06354cfa926d1e35755f0
Matt Milburn <me@mattmilburn.com>
2023-08-13 14:19:54 -0500

    Restore parseUrl server util

---
+++ server/utils/parse-url.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const interpolate = require('./interpolate');
+const trimSlashes = require('./trim-slashes');
+
+const parseUrl = (config, data) => {
+  if (!config?.url || !data) {
+    return null;
+  }
+
+  const supportedTypes = ['number', 'string'];
+  const replacements = Object.entries(data).reduce((acc, [key, val]) => {
+    if (!supportedTypes.includes(typeof val)) {
+      return acc;
+    }
+
+    return {
+      ...acc,
+      [key]: val,
+    };
+  }, {});
+
+  const url = interpolate(trimSlashes(config.url), replacements);
+
+  return url;
+};
+
+module.exports = parseUrl;
