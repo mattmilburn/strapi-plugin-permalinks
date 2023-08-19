0d49c9decba22b3cb6474d90d18fbb9281ec6ba7
Matt Milburn <me@mattmilburn.com>
2023-08-19 13:30:03 -0500

    Add custom field icon

--- admin/src/components/index.js
+++ admin/src/components/index.js
@@ -1,4 +1,5 @@
 export { default as CopyLinkButton } from './CopyLinkButton';
+export { default as CustomFieldIcon } from './CustomFieldIcon';
 export { default as EditViewRightLinks } from './EditViewRightLinks';
 export { default as ListViewColumn } from './ListViewColumn';
 export { default as PermalinkInput } from './PermalinkInput';
